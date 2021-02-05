4857ea3d5691df2f8628510917fdf7170eaa32ee
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-02-05 17:29:26 +0000

    added why ICC

--- app/src/main/java/uk/ac/swansea/alexandru/dvmicc/WelcomeActivity.kt
+++ app/src/main/java/uk/ac/swansea/alexandru/dvmicc/WelcomeActivity.kt
@@ -2,10 +2,15 @@ package uk.ac.swansea.alexandru.dvmicc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val whyICCtext : TextView = findViewById<TextView>(R.id.whyICCtext)
+        whyICCtext.movementMethod = LinkMovementMethod.getInstance()
     }
 }
