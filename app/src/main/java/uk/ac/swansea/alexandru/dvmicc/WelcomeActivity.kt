2f13a4519b18d871bd5b8426583fa1f202f9b4c5
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-02-10 19:19:21 +0000

    added code snippets

--- app/src/main/java/uk/ac/swansea/alexandru/dvmicc/WelcomeActivity.kt
+++ app/src/main/java/uk/ac/swansea/alexandru/dvmicc/WelcomeActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 
+import io.github.kbiakov.codeview.CodeView
+import io.github.kbiakov.codeview.adapters.Options
+
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +15,17 @@ class WelcomeActivity : AppCompatActivity() {
 
         val whyICCtext : TextView = findViewById<TextView>(R.id.whyICCtext)
         whyICCtext.movementMethod = LinkMovementMethod.getInstance()
+
+        val codeView : CodeView = findViewById(R.id.code_view)
+        codeView.setOptions(Options.Default.get(this)
+                .withLanguage("java")
+                .withCode(getString(R.string.implicitIntentCode))
+                .disableHighlightAnimation())
+
+        val codeViewXML : CodeView = findViewById(R.id.code_viewXML)
+        codeViewXML.setOptions(Options.Default.get(this)
+            .withLanguage("xml")
+            .withCode(getString(R.string.intentFilterCode))
+            .disableHighlightAnimation())
     }
 }
