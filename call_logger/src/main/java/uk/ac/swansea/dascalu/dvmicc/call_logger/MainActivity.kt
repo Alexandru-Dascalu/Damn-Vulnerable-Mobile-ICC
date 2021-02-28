6a106e2634c8af923840547b1bf6caa1dd614fff
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-02-28 15:40:39 +0000

    change call logger colors

--- call_logger/src/main/java/uk/ac/swansea/dascalu/dvmicc/call_logger/MainActivity.kt
+++ call_logger/src/main/java/uk/ac/swansea/dascalu/dvmicc/call_logger/MainActivity.kt
@@ -3,9 +3,14 @@ package uk.ac.swansea.dascalu.dvmicc.call_logger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.google.android.material.appbar.MaterialToolbar
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainAppBar = findViewById<MaterialToolbar>(R.id.mainActivityToolbar)
+        setSupportActionBar(mainAppBar)
     }
 }
