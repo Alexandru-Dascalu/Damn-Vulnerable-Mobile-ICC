3a25cb5cc695ff0ed9c0b7f12466ae169e141e19
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-02-28 14:49:05 +0000

    added malware

---
+++ call_logger/src/main/java/uk/ac/swansea/dascalu/dvmicc/call_logger/MainActivity.kt
@@ -0,0 +1,11 @@
+package uk.ac.swansea.dascalu.dvmicc.call_logger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
