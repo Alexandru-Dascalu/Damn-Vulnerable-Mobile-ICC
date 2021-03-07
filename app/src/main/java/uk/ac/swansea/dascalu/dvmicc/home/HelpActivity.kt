b0de711e153488f55f1e97e3f59b04bce1c4b6ea
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-03-07 12:17:26 +0000

    added level descriptions

--- app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/HelpActivity.kt
+++ app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/HelpActivity.kt
@@ -1,6 +1,7 @@
 package uk.ac.swansea.dascalu.dvmicc.home
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.github.kbiakov.codeview.CodeView
 import uk.ac.swansea.dascalu.dvmicc.home.model.Challenge
@@ -9,19 +10,37 @@ import java.lang.IllegalStateException
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_broadcast_theft_help)
+        setContentView(R.layout.activity_help)
 
         if(intent.extras != null) {
             val challenge: Challenge? = intent.extras!!.getSerializable("challenge") as Challenge?
 
             if(challenge != null) {
                 setCode(challenge)
+                setLevelDescriptions(challenge)
             } else {
                 throw IllegalStateException("No challenge in intent for help activity!")
             }
         } else {
             throw IllegalStateException("No extras in intent that started help activity!")
         }
+    }
+
+    private fun setLevelDescriptions(challenge: Challenge) {
+        val securityLowTextView = findViewById<TextView>(R.id.securityLowDescription)
+        securityLowTextView.text = getString(challenge.securityLevels["low"]!!.explanationID)
+
+        val securityMediumTextView = findViewById<TextView>(R.id.securityMediumDescription)
+        securityMediumTextView.text = getString(challenge.securityLevels["medium"]!!.explanationID)
+
+        val securityHighTextView = findViewById<TextView>(R.id.securityHighDescription)
+        securityHighTextView.text = getString(challenge.securityLevels["high"]!!.explanationID)
+
+        val securityVeryHighTextView = findViewById<TextView>(R.id.securityVeryHighDescription)
+        securityVeryHighTextView.text = getString(challenge.securityLevels["very high"]!!.explanationID)
+
+        val securityImpossibleTextView = findViewById<TextView>(R.id.securityImpossibleDescription)
+        securityImpossibleTextView.text = getString(challenge.securityLevels["impossible"]!!.explanationID)
     }
 
     private fun setCode(challenge: Challenge) {
