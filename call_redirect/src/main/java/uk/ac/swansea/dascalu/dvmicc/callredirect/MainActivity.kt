b15bbd21aa674f6ad4f74478df962191bb3104c9
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-03-12 10:24:57 +0000

    saving code to prefrences

--- call_redirect/src/main/java/uk/ac/swansea/dascalu/dvmicc/callredirect/MainActivity.kt
+++ call_redirect/src/main/java/uk/ac/swansea/dascalu/dvmicc/callredirect/MainActivity.kt
@@ -1,11 +1,23 @@
 package uk.ac.swansea.dascalu.dvmicc.callredirect
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun saveCountryCode(view: View) {
+        val countryCodeEditText = findViewById<EditText>(R.id.countryCodeEditText)
+        val sharedPreferences = getSharedPreferences("country_code", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        editor.putString("country_code", "+${countryCodeEditText.text}")
+        editor.apply()
     }
 }
