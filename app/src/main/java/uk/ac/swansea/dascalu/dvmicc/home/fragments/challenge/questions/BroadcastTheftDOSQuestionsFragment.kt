6b0b8231d851558c5e6c735ffc13b04083c96166
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-03-17 16:45:43 +0000

    hiding unused security Levels

--- app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/fragments/challenge/questions/BroadcastTheftDOSQuestionsFragment.kt
+++ app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/fragments/challenge/questions/BroadcastTheftDOSQuestionsFragment.kt
@@ -3,6 +3,11 @@ package uk.ac.swansea.dascalu.dvmicc.home.fragments.challenge.questions
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+
+import com.google.android.material.textfield.TextInputLayout
+
 import uk.ac.swansea.dascalu.dvmicc.home.R
 
 @SuppressLint("UseRequireInsteadOfGet")
@@ -18,12 +23,23 @@ class BroadcastTheftDOSQuestionsFragment : AbstractFullQuestionsFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         challengeName = context!!.resources.getString(R.string.broadcastTheftDOSName)
         vulnerableAppName = context!!.resources.getString(R.string.callRedirectAppName)
-        malwareName = context!!.resources.getString(R.string.callLogAppName)
+        malwareName = context!!.resources.getString(R.string.cpuBoosterAppName)
 
         val flags = context!!.resources.getStringArray(R.array.broadcastTheftDOSFlags)
         securityLowFlag = flags[0]
         securityHighFlag = flags[1]
 
         super.onViewCreated(view, savedInstanceState)
+        hideElementsForUnusedSecurityLevels(view)
+    }
+
+    private fun hideElementsForUnusedSecurityLevels(view: View) {
+        view.findViewById<TextView>(R.id.securityMediumQuestionTextView).visibility = View.GONE
+        view.findViewById<TextInputLayout>(R.id.securityMediumInput).visibility = View.GONE
+        view.findViewById<MaterialButton>(R.id.securityMediumButton).visibility = View.GONE
+
+        view.findViewById<TextView>(R.id.securityVeryHighQuestionTextView).visibility = View.GONE
+        view.findViewById<TextInputLayout>(R.id.securityVeryHighInput).visibility = View.GONE
+        view.findViewById<MaterialButton>(R.id.securityVeryHighButton).visibility = View.GONE
     }
 }
