c6f7d4e3d1728f4b0654c70ac04502328cf70530
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-03-17 15:26:23 +0000

    made app build again

--- app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/utils.kt
+++ app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/utils.kt
@@ -6,11 +6,14 @@ import io.github.kbiakov.codeview.adapters.Format
 import io.github.kbiakov.codeview.adapters.Options
 import io.github.kbiakov.codeview.highlight.ColorTheme
 
-fun setCodeViewOptions(context: Context, codeView: CodeView, language: String, codeStringID: Int) {
-    codeView.setOptions(Options.Default.get(context)
-            .withLanguage(language)
-            .withCode(context.getString(codeStringID))
-            .withTheme(ColorTheme.MONOKAI)
-            .withFormat(Format.ExtraCompact)
-            .disableHighlightAnimation())
+fun setCodeViewOptions(context: Context, codeView: CodeView, language: String, codeStringID: Int?) {
+    //check if there actually is code to place in the code view
+    if(codeStringID != null) {
+        codeView.setOptions(Options.Default.get(context)
+                .withLanguage(language)
+                .withCode(context.getString(codeStringID))
+                .withTheme(ColorTheme.MONOKAI)
+                .withFormat(Format.ExtraCompact)
+                .disableHighlightAnimation())
+    }
 }
