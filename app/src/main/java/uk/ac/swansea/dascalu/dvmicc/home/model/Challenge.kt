42f5e25c448217cc3dc99fbd234f4cb50c7bdef6
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-03-18 18:13:09 +0000

    challenge object refactoring

--- app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/model/Challenge.kt
+++ app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/model/Challenge.kt
@@ -2,23 +2,24 @@ package uk.ac.swansea.dascalu.dvmicc.home.model
 
 import uk.ac.swansea.dascalu.dvmicc.home.R
 
-enum class Challenge(val challengeNameIndex: Int, val securityLevels: Map<String, SecurityLevel>,
-                     val attackExplanation: Int, val instructions: Int,
-                     val scenarioExplanation: Int, val apiRequirements: Int) {
+enum class Challenge(val challengeNameIndex: Int, val attackExplanation: Int, val securityLevels: Map<String, SecurityLevel>,
+                     val instructions: Int, val scenarioExplanation: Int, val apiRequirements: Int) {
 
-    BROADCAST_THEFT(0,
+    BROADCAST_THEFT(0,  R.string.broadcastTheftExplanation,
             mapOf("low" to SecurityLevel(R.string.broadcastTheftLowDescription, R.string.newsAggregatorLowManifest, R.string.newsAggregatorLowIntentCode),
                     "medium" to SecurityLevel(R.string.broadcastTheftMediumDescription, R.string.newsAggregatorMediumManifest, R.string.newsAggregatorMediumIntentCode),
                     "high" to SecurityLevel(R.string.broadcastTheftHighDescription, R.string.newsAggregatorHighManifest, R.string.newsAggregatorMediumIntentCode),
                     "very high" to SecurityLevel(R.string.broadcastTheftVeryHighDescription, R.string.newsAggregatorVeryHighManifest, R.string.newsAggregatorMediumIntentCode),
                     "impossible" to SecurityLevel(R.string.broadcastTheftImpossibleDescription, R.string.newsAggregatorLowManifest, R.string.newsAggregatorImpossibleIntentCode)),
-            R.string.broadcastTheftExplanation, R.string.broadcastTheftInstructions,
-            R.string.broadcastTheftScenarioExplanation, R.string.broadcastTheftAPIRequirements),
+            R.string.broadcastTheftInstructions, R.string.broadcastTheftScenarioExplanation,
+            R.string.broadcastTheftAPIRequirements),
 
-    BROADCAST_THEFT_DOS(1,
+    BROADCAST_THEFT_DOS(1, R.string.broadcastTheftDOSExplanation,
             mapOf("low" to SecurityLevel(R.string.broadcastTheftDOSLowDescription, R.string.callRedirectLowManifest, R.string.callRedirectLowIntentCode),
                     "high" to SecurityLevel(R.string.broadcastTheftDOSHighDescription, R.string.callRedirectHighManifest, R.string.callRedirectHighIntentCode),
                     "impossible" to SecurityLevel(R.string.broadcastTheftDOSImpossibleDescription)),
-            R.string.broadcastTheftDOSExplanation, R.string.broadcastTheftDOSInstructions, 0, 0)
+            R.string.broadcastTheftDOSInstructions, 0, 0)
 
 }
+
+data class SecurityLevel(val explanationID: Int, val manifestID: Int? = null, val intentCodeID: Int? = null)
--- app/src/main/java/uk/ac/swansea/dascalu/dvmicc/home/model/SecurityLevel.kt
+++
@@ -1,3 +0,0 @@
-package uk.ac.swansea.dascalu.dvmicc.home.model
-
-data class SecurityLevel(val explanationID: Int, val manifestID: Int? = null, val intentCodeID: Int? = null)
