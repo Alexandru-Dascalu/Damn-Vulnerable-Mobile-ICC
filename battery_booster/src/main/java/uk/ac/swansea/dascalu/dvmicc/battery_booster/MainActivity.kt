435241d6aebefef37569422b18e9e0646658a532
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-03-31 15:40:49 +0100

    optimising battery

--- battery_booster/src/main/java/uk/ac/swansea/dascalu/dvmicc/battery_booster/MainActivity.kt
+++ battery_booster/src/main/java/uk/ac/swansea/dascalu/dvmicc/battery_booster/MainActivity.kt
@@ -8,26 +8,33 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.google.android.material.snackbar.Snackbar
-import java.util.*
+
+import java.util.Random
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     private val random = Random()
-    private var boostProgress : Int = random.nextInt(15)
+    //duration is measured in seconds
+    private var optimiseDuration : Int = 0
+    private var startOptimiseTime : Long = 0
+    private var isOptimised : Boolean = false
     private var timer : Timer? = null
 
     private val requestPermissionsLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()) { granted: Boolean ->
 
         if(granted) {
-            Snackbar.make(findViewById(R.id.progressLevelTextView),
+            Snackbar.make(findViewById(R.id.optimiseTextView),
                     R.string.storagePermissionAcquired, Snackbar.LENGTH_LONG).show()
         } else {
-            Snackbar.make(findViewById(R.id.progressLevelTextView),
+            Snackbar.make(findViewById(R.id.optimiseTextView),
                     R.string.fileStoragePermissionWarning, Snackbar.LENGTH_LONG).show()
         }
     }
@@ -36,7 +43,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        updateProgressBar()
+        setBatteryIssues()
+        stopProgressBar()
         getStoragePermission()
     }
 
@@ -63,31 +71,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setBatteryIssues() {
+        val issues : Int = random.nextInt(25)
+        findViewById<TextView>(R.id.optimiseTextView).text = resources.getString(
+                R.string.battery_issues_text, issues)
+    }
+
     fun startProgress(view: View) {
-        timer = Timer()
+        if(!isOptimised) {
+            optimiseDuration = random.nextInt(20)
+            startOptimiseTime = System.currentTimeMillis()
 
-        val task = object : TimerTask() {
-            override fun run() {
-                if(boostProgress != 100) {
-                    val inc = random.nextInt(10)
-                    boostProgress += inc
+            val progressBar = findViewById<LinearProgressIndicator>(R.id.progressBar)
+            progressBar.showAnimationBehavior = LinearProgressIndicator.SHOW_INWARD
+            progressBar.visibility = View.VISIBLE
 
-                    if(boostProgress > 100) {
-                        boostProgress = 100
+
+            timer = Timer()
+
+            val task = object : TimerTask() {
+                override fun run() {
+                    if(System.currentTimeMillis() - startOptimiseTime >= optimiseDuration * 1000) {
+                        runOnUiThread {
+                            stopProgressBar()
+                            findViewById<TextView>(R.id.optimiseTextView).text = resources.getString(
+                                    R.string.optimsed_text)
+                        }
+
+                        isOptimised = true
+                        timer!!.cancel()
+                        timer!!.purge()
+                        timer = null
                     }
-
-                    runOnUiThread {
-                        updateProgressBar()
-                    }
-                } else {
-                    timer!!.cancel()
-                    timer!!.purge()
-                    timer = null
                 }
             }
-        }
 
-        timer!!.schedule(task, 300, 300)
+            timer!!.schedule(task, 10, 300)
+        }
     }
 
     fun startAdvancedActivity(view: View) {
@@ -95,16 +115,9 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun updateProgressBar() {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        progressBar.progress = boostProgress
-
-        val progressTextView = findViewById<TextView>(R.id.progressLevelTextView)
-
-        if(boostProgress == 100) {
-            progressTextView.text = "$boostProgress%\nPhone optimised"
-        } else {
-            progressTextView.text = "$boostProgress%"
-        }
+    private fun stopProgressBar() {
+        val progressBar = findViewById<LinearProgressIndicator>(R.id.progressBar)
+        progressBar.showAnimationBehavior = LinearProgressIndicator.SHOW_NONE
+        progressBar.visibility = View.INVISIBLE
     }
 }
