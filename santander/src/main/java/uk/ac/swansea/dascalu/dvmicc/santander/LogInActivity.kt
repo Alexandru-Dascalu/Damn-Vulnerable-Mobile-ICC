0ba37f968c3cf9aff27a82406d29e44e0dde7055
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-04-03 17:22:43 +0100

    fixed log in

--- santander/src/main/java/uk/ac/swansea/dascalu/dvmicc/santander/LogInActivity.kt
+++ santander/src/main/java/uk/ac/swansea/dascalu/dvmicc/santander/LogInActivity.kt
@@ -2,11 +2,13 @@ package uk.ac.swansea.dascalu.dvmicc.santander
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.snackbar.Snackbar
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,16 +19,16 @@ class LogInActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val customerIDEditText = findViewById<EditText>(R.id.customerIDInput)
-        customerIDEditText.setOnEditorActionListener { _, actionId, _ ->
-            return@setOnEditorActionListener checkLogin(actionId)
+        customerIDEditText.setOnEditorActionListener { view , actionId, _ ->
+            return@setOnEditorActionListener checkLogin(view, actionId)
         }
 
-        findViewById<EditText>(R.id.passwordInput).setOnEditorActionListener { _, actionId, _ ->
-            return@setOnEditorActionListener checkLogin(actionId)
+        findViewById<EditText>(R.id.passwordInput).setOnEditorActionListener { view, actionId, _ ->
+            return@setOnEditorActionListener checkLogin(view, actionId)
         }
     }
 
-    private fun checkLogin(actionID: Int) : Boolean {
+    private fun checkLogin(view: View, actionID: Int) : Boolean {
         if(actionID == EditorInfo.IME_ACTION_DONE) {
             val customerIDInput = findViewById<EditText>(R.id.customerIDInput)
             val passwordInput = findViewById<EditText>(R.id.passwordInput)
@@ -42,6 +44,9 @@ class LogInActivity : AppCompatActivity() {
                 passwordInput.text.clear()
 
                 return true
+            } else {
+                Snackbar.make(view, "Log In details incorrect!", Snackbar.LENGTH_LONG).show()
+                return false
             }
         }
 
