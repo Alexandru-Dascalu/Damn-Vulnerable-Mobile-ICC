d135ae9c379a42a8fe4ee8e41f45937fcdc80bf1
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-04-04 18:08:08 +0100

    added hamburger menu

--- money_manager/src/main/java/uk/ac/swansea/dascalu/dvmicc/moneymanager/MainActivity.kt
+++ money_manager/src/main/java/uk/ac/swansea/dascalu/dvmicc/moneymanager/MainActivity.kt
@@ -2,10 +2,20 @@ package uk.ac.swansea.dascalu.dvmicc.moneymanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topAppBar = findViewById<MaterialToolbar>(R.id.mainActivityToolbar)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        setSupportActionBar(topAppBar)
+
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
+        }
     }
 }
