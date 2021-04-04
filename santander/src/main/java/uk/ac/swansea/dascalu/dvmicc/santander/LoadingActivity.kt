8fdfe7ba446ba5b96f40d087f03d0fbce2e84b2c
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-04-04 11:27:20 +0100

    added implicit intent

--- santander/src/main/java/uk/ac/swansea/dascalu/dvmicc/santander/LoadingActivity.kt
+++ santander/src/main/java/uk/ac/swansea/dascalu/dvmicc/santander/LoadingActivity.kt
@@ -1,6 +1,7 @@
 package uk.ac.swansea.dascalu.dvmicc.santander
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -25,7 +26,7 @@ class LoadingActivity : AppCompatActivity() {
                     timer!!.purge()
                     timer = null
 
-                    val intent = Intent(this@LoadingActivity, LogInActivity::class.java)
+                    val intent = makeLogInIntent()
                     startActivity(intent)
                     this@LoadingActivity.finish()
                 }
@@ -33,5 +34,17 @@ class LoadingActivity : AppCompatActivity() {
         }
 
         timer!!.schedule(task, 10, 2500)
+    }
+
+    private fun makeLogInIntent() : Intent {
+        val noPaymentUri : Uri = Uri.parse("santander_pay://pay")
+
+        val intent = Intent()
+        intent.action = "uk.ac.swansea.dascalu.dvmicc.santander.intent.action.LOGIN"
+        intent.addCategory("android.intent.category.HOME")
+        intent.type = "text/plain"
+        intent.data = noPaymentUri
+
+        return intent
     }
 }
