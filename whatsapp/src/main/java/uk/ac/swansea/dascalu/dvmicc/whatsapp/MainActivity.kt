034028fb92626060ef4f6856cd5162e441b4ae3e
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-04-11 14:58:32 +0100

    added appbar

--- whatsapp/src/main/java/uk/ac/swansea/dascalu/dvmicc/whatsapp/MainActivity.kt
+++ whatsapp/src/main/java/uk/ac/swansea/dascalu/dvmicc/whatsapp/MainActivity.kt
@@ -2,10 +2,14 @@ package uk.ac.swansea.dascalu.dvmicc.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appbar = findViewById<MaterialToolbar>(R.id.mainActivityToolbar)
+        setSupportActionBar(appbar)
     }
 }
