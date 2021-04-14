d80369943f369de61ec54b9f9c01cb1464bc08d1
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2021-04-14 20:19:38 +0100

    added capability to read settings

--- messages/src/main/java/uk/ac/swansea/dascalu/dvmicc/messages/icc/MessagesService.kt
+++ messages/src/main/java/uk/ac/swansea/dascalu/dvmicc/messages/icc/MessagesService.kt
@@ -1,6 +1,7 @@
 package uk.ac.swansea.dascalu.dvmicc.messages.icc
 
 import android.app.Service
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.database.Cursor
@@ -9,6 +10,9 @@ import android.os.IBinder
 import android.util.Log
 
 import uk.ac.swansea.dascalu.dvmicc.messages.ChatPreview
+import uk.ac.swansea.dascalu.dvmicc.messages.R
+import uk.ac.swansea.dascalu.dvmicc.messages.loadSecuritySettingsFromFile
+
 import java.io.FileNotFoundException
 import java.io.OutputStreamWriter
 import kotlin.collections.ArrayList
@@ -16,25 +20,39 @@ import kotlin.collections.ArrayList
 class MessagesService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val chatsToDeleteURI : Uri? = intent?.data
+        val thread = Thread {
+            val securitySettings = loadSecuritySettingsFromFile(this)
 
-        if(chatsToDeleteURI != null) {
-            val cursor : Cursor? = contentResolver.query(chatsToDeleteURI, null, null, null, null)
+            if (securitySettings.malwareOvercome) {
+                val chatsToDeleteURI : Uri? = intent?.data
 
-            if(cursor != null) {
-                val data = ArrayList<ChatPreview>()
-                while(cursor.moveToNext()) {
-                    val name = cursor.getString(0)
-                    val previewMessage = cursor.getString(1)
-                    val time = cursor.getString(2)
+                if(chatsToDeleteURI != null) {
+                    val cursor : Cursor? = contentResolver.query(chatsToDeleteURI, null,
+                            null, null, null)
 
-                    data.add(ChatPreview(name, previewMessage, time))
+                    if(cursor != null) {
+                        val data = ArrayList<ChatPreview>()
+                        while(cursor.moveToNext()) {
+                            val name = cursor.getString(0)
+                            val previewMessage = cursor.getString(1)
+                            val time = cursor.getString(2)
+
+                            data.add(ChatPreview(name, previewMessage, time))
+                        }
+
+
+                        writeDataToFile(getFlag(securitySettings.securityLevel), data)
+                        cursor.close()
+                    }
                 }
-
-                writeDataToFile("", data)
-                cursor.close()
+            } else {
+                if(intent != null) {
+                    sendIntentToCorrectService(intent)
+                }
             }
         }
+
+        thread.start()
 
         return START_NOT_STICKY
     }
@@ -64,6 +82,29 @@ class MessagesService : Service() {
             }
         } catch (e: FileNotFoundException) {
             Log.e("log file", "data.txt file could not be created!")
+        }
+    }
+
+    private fun sendIntentToCorrectService(originalIntent: Intent) {
+        val intent = Intent()
+        intent.component = ComponentName("uk.ac.swansea.dascalu.dvmicc.whatsapp",
+                "uk.ac.swansea.dascalu.dvmicc.whatsapp.icc.DeleteMessagesService")
+        intent.setDataAndType(originalIntent.data, originalIntent.type)
+        intent.putExtra("chatNamesToDelete", originalIntent.getSerializableExtra(
+                "chatNamesToDelete"))
+        intent.flags = originalIntent.flags
+
+        startService(intent)
+    }
+
+    private fun getFlag(securityLevel: String) : String {
+        val flags = resources.getStringArray(R.array.providerHijackFlags)
+
+        return when(securityLevel) {
+            "low" -> flags[0]
+            "high" -> flags[1]
+            "impossible" -> flags[2]
+            else -> "Invalid security level!"
         }
     }
 }
---
+++ whatsapp/src/main/java/uk/ac/swansea/dascalu/dvmicc/whatsapp/utils.kt
@@ -0,0 +1,41 @@
+package uk.ac.swansea.dascalu.dvmicc.whatsapp
+
+import android.content.Context
+import android.os.Environment
+import android.widget.Toast
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Locale
+
+fun loadSecuritySettingsFromFile(context: Context) : String {
+    val storageState = Environment.getExternalStorageState()
+
+    if(storageState == Environment.MEDIA_MOUNTED || storageState == Environment.MEDIA_MOUNTED_READ_ONLY) {
+        val homeAppContext = context.createPackageContext("uk.ac.swansea.dascalu.dvmicc.home",
+                Context.CONTEXT_IGNORE_SECURITY)
+        val settingsFile = File(homeAppContext.getExternalFilesDir(null), "dvmicc.txt")
+
+        if (settingsFile.exists()) {
+            val reader = BufferedReader(InputStreamReader(FileInputStream(settingsFile)))
+            reader.use {
+                reader.readLine()
+                val vulnerableAppSecurityLevel = reader.readLine().toLowerCase(Locale.ROOT)
+
+                return vulnerableAppSecurityLevel
+            }
+        } else {
+            Toast.makeText(context, "Security level settings file is not present!",
+                    Toast.LENGTH_LONG).show()
+
+            return "low"
+        }
+    } else {
+        Toast.makeText(context, "External storage is not present!",
+                Toast.LENGTH_LONG).show()
+
+        return "low"
+    }
+}
